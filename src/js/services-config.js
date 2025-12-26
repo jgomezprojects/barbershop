91d6ec4f8f84fdb42656d0ddac9b665abf89ff3c
juan gomez <juangomez.devp@gmail.com>
2025-12-26 08:04:28 -0500

    Add files via upload

---
+++ src/js/services-config.js
@@ -0,0 +1,49 @@
+// Configuración de servicios con duraciones (en minutos)
+const SERVICES_CONFIG = {
+    'corte': {
+        name: 'Corte de Cabello',
+        duration: 30,
+        price: 25
+    },
+    'barba': {
+        name: 'Afeitado de Barba',
+        duration: 20,
+        price: 15
+    },
+    'corte-barba': {
+        name: 'Corte y Barba',
+        duration: 45,
+        price: 35
+    },
+    'corte-barba-cejas': {
+        name: 'Corte, Barba y Cejas',
+        duration: 50,
+        price: 35
+    },
+    'corte-barba-depilacion': {
+        name: 'Corte, Barba y Depilación',
+        duration: 60,
+        price: 40
+    },
+    'corte-barba-exfoliacion': {
+        name: 'Corte, Barba y Exfoliación',
+        duration: 60,
+        price: 40
+    },
+    'completo': {
+        name: 'Paquete Completo',
+        duration: 90,
+        price: 45
+    },
+    'exfoliacion-depilacion': {
+        name: 'Exfoliación y Depilación',
+        duration: 40,
+        price: 25
+    },
+    'mascara-led': {
+        name: 'Máscara LED Ultra Sónica',
+        duration: 45,
+        price: 50
+    }
+};
+
